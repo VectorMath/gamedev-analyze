c0281adc955c9353b59be4c8a90052da420b4281
VectorMath <ev.zharov1998@gmail.com>
2026-05-19 08:38:23 +0300

    Added comments to constants.py

--- dags/src/constants.py
+++ dags/src/constants.py
@@ -1,4 +1,8 @@
-DB_CONNECTION_ID: str = "postgres_db"
+"""
+The python file with constants for DAGs and common function
+"""
 
-UPSERT_DATA_TO_CORE_TASK_ID: str = "upsert_data_to_core"
-UPDATE_HWM_TASK_ID: str = "update_high_watermark"
+DB_CONNECTION_ID: str = "postgres_db" # The ID of postgres connection for Airflow
+
+UPSERT_DATA_TO_CORE_TASK_ID: str = "upsert_data_to_core" # The task ID for upsert data to core layer.
+UPDATE_HWM_TASK_ID: str = "update_high_watermark" # The task ID for update high_watermark table.
